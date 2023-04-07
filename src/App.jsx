daa51f9000388d01dc68bdf3bc96fa8a39fc2d14
Minami <minami.314m@gmail.com>
2023-04-07 01:32:06 +0000

    Udemy_61の変更点

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,10 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {/* インラインでstyleを定義する */}
+      {incompleteTodos.length >= 5 && <p style={{ color: "red" }}>上限5個</p>}
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
